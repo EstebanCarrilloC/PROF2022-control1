4613daebd4c509bede7eb113879853ecf57cf890
Esteban Carrillo <me.carrillo@alumnos.upm.es>
2022-11-07 18:16:41 +0100

    Add Maven project with simple archetype

---
+++ src/main/java/es/upm/mecarrillo/profundizacion2022/PROF2022_control1/App.java
@@ -0,0 +1,13 @@
+package es.upm.mecarrillo.profundizacion2022.PROF2022_control1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
